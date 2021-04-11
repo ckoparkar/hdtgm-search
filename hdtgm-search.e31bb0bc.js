1cf3d08594f0d879b6597b8cb06816b128308c4c
Chaitanya Koparkar <ckoparkar@gmail.com>
2021-04-11 19:00:00 -0400

    Update

--- hdtgm-search.e31bb0bc.js
+++ hdtgm-search.e31bb0bc.js
@@ -1887,6 +1887,60 @@ var db = [{
   directors: ["Pat O'Connor"],
   actors: ["Kevin Kline", "Susan Sarandon", "Mary Elizabeth Mastrantonio", "Harvey Keitel", "Danny Aiello", "Rod Steiger"]
 }, {
+  id: 262,
+  name: "Zack Snyder’s Justice League ",
+  guests: ["Griffin Newman", "David Sims"],
+  directors: ["Zack Snyder"],
+  actors: ["Ben Affleck", "Henry Cavill", "Amy Adams", "Gal Gadot", "Ray Fisher", "Jason Momoa", "Ezra Miller", "Willem Dafoe", "Jesse Eisenberg", "Jeremy Irons", "Diane Lane", "Connie Nielsen", "J. K. Simmons"]
+}, {
+  id: 261,
+  name: "A Talking Cat!?!",
+  guests: ["Jessica St. Clair"],
+  directors: ["David DeCoteau"],
+  actors: ["Johnny Whitaker", "Kristine DeBell", "Justin Cone", "Janis Peebles", "Alison Sieke", "Daniel Dannas", "Eric Roberts"]
+}, {
+  id: 260,
+  name: "Jade",
+  guests: [],
+  directors: ["William Friedkin"],
+  actors: ["David Caruso", "Linda Fiorentino", "Chazz Palminteri", "Michael Biehn", "Richard Crenna"]
+}, {
+  id: 259,
+  name: "Scientology In Hollywood",
+  guests: ["Leah Remini", "Mike Rinder"],
+  directors: [],
+  actors: []
+}, {
+  id: 258,
+  name: "Holy Matrimony",
+  guests: [],
+  directors: ["Leonard Nimoy"],
+  actors: ["Patricia Arquette", "Joseph Gordon-Levitt", "Tate Donovan", "Armin Mueller-Stahl"]
+}, {
+  id: 257,
+  name: "Lawnmower Man 2: Beyond Cyberspace",
+  guests: [],
+  directors: ["Farhad Mann"],
+  actors: ["Patrick Bergin", "Matt Frewer", "Austin O'Brien", "Ely Pouget", "Camille Cooper", "Kevin Conway"]
+}, {
+  id: 256,
+  name: "A Gnome Named Gnorm",
+  guests: [],
+  directors: ["Stan Winston"],
+  actors: ["Anthony Michael Hall", "Jerry Orbach", "Claudia Christian", "Eli Danker", "Mark Harelik", "Robert Z'Dar", "Pat Crawford Brown", "Joseph R. Sicari", "Greg Kean"]
+}, {
+  id: 255,
+  name: "A Very Nutty Christmas ",
+  guests: ["Jessica St. Clair"],
+  directors: ["Colin Theys"],
+  actors: ["Melissa Joan Hart", "Barry Watson", "Marissa Jaret Winokur "]
+}, {
+  id: 254,
+  name: "Love's Labour's Lost",
+  guests: [],
+  directors: ["Kenneth Branagh"],
+  actors: ["Kenneth Branagh", "Nathan Lane", "Adrian Lester", "Matthew Lillard", "Natascha McElhone", "Alessandro Nivola", "Alicia Silverstone", "Timothy Spall"]
+}, {
   id: 205,
   name: "Cellular",
   guests: ["Ike Barinholtz", "Erin Gibson"],
@@ -2047,10 +2101,16 @@ var movieDb = require("./db");
     directors.classList.add("movie-directors");
     guests.classList.add("movie-guests");
     name.textContent = "#" + movie.id + " " + movie.name;
-    actors.textContent = "Starring: " + movie.actors.join(", ");
-    directors.textContent = "Directed By: " + movie.directors.join(", ");
 
-    if (movie.guests != null) {
+    if (movie.actors.length != 0) {
+      actors.textContent = "Starring: " + movie.actors.join(", ");
+    }
+
+    if (movie.directors.length != 0) {
+      directors.textContent = "Directed By: " + movie.directors.join(", ");
+    }
+
+    if (movie.guests.length != 0) {
       guests.textContent = "Episode Guests: " + movie.guests.join(", ");
     }
 
@@ -2111,7 +2171,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57604" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58307" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
